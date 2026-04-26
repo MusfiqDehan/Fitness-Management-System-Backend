4d0fd892f83b5c897c09c3b6d96b73b11a43d35b
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2026-04-26 15:50:32 +0600

    feat: update login endpoint to use EmailOrPhoneTokenObtainPairView and add API schema documentation routes

--- gym_back_end/urls.py
+++ gym_back_end/urls.py
@@ -4,9 +4,10 @@ from django.views.static import serve
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
 )
+from accounts.views import EmailOrPhoneTokenObtainPairView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 
 urlpatterns = [
@@ -15,8 +16,13 @@ urlpatterns = [
     path('api/accounts/', include('accounts.urls')),
     path('api/dashboard/', include('dashboard.urls')),
     path('api/membership/', include('membership_management.urls')),
-    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/login/', EmailOrPhoneTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # API Schema & Docs
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
