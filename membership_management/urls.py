f416da6e060774e4b3a74e29ab4805f6cac449a9
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2026-04-29 16:10:39 +0600

    feat(membership_management): remove membership management app and all related files

--- membership_management/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import CheckAccessAPIView,MembersInsideAPIView
-
-urlpatterns = [
-    path('gym/check-access/', CheckAccessAPIView.as_view()),
-    path('gym/members-inside/', MembersInsideAPIView.as_view()),
-]
