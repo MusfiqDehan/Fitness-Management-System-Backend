9be5efb1e2a47f0a45ef3ff811760d93e14a8a4b
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2026-05-03 12:33:38 +0600

    feat(membership):rename membership_management app to membership

--- apps/dashboard/tests.py
+++ apps/dashboard/tests.py
@@ -6,7 +6,7 @@ from rest_framework.test import APITestCase
 
 from apps.identity.models import User
 from apps.quick_action.models import BlogCategory, Category, ClassSchedule, Contact, GymClass
-from apps.membership_management.models import Member, MemberPackage
+from apps.membership.models import Member, MemberPackage
 
 
 class DashboardAPIViewTests(APITestCase):
--- apps/membership_management/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class MembershipManagementConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.membership_management'
-    label = 'membership_management'
