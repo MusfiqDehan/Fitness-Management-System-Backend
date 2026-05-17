48b0e8083d02ac791f746ca9dca45954887aabe9
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2026-05-17 17:42:35 +0600

    feat: register ContactQuery and EmailConfig models in admin interface

--- apps/crm/admin.py
+++ apps/crm/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ContactQuery, EmailConfig
+
+
+@admin.register(ContactQuery)
+class ContactQueryAdmin(admin.ModelAdmin):
+    list_display = ["full_name", "phone_number", "email", "package_name", "created_at"]
+    list_filter = ["created_at"]
+    search_fields = ["full_name", "phone_number", "email"]
+    readonly_fields = ["created_at"]
+
+
+@admin.register(EmailConfig)
+class EmailConfigAdmin(admin.ModelAdmin):
+    list_display = ["name", "host", "port", "host_user", "is_active", "created_at"]
+    list_filter = ["is_active", "use_tls", "use_ssl"]
+    search_fields = ["name", "host_user", "host"]
+    readonly_fields = ["created_at", "updated_at", "created_by", "updated_by"]
+
