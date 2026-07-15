006a6df662f2c80c68a2ef7ec7b6b3f79d64921d
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2026-07-15 13:28:27 +0600

    feat(settings): update JWT authentication to use RevocationAwareJWTAuthentication and adjust token lifetimes for enhanced security

--- config/settings.py
+++ config/settings.py
@@ -422,7 +422,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         # "rest_framework.authentication.SessionAuthentication",
         # "rest_framework.authentication.TokenAuthentication",
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "utils.jwt_authentication.RevocationAwareJWTAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         # "rest_framework.permissions.IsAuthenticated",
@@ -467,8 +467,9 @@ REST_FRAMEWORK = {
     "PAGE_SIZE": 10,
 }
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=2),   
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7), 
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ROTATE_REFRESH_TOKENS': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 # user model
